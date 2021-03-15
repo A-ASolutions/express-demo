69d533b9170d0927c67f9e337816ec0f82c0579e
aalshoaibi <69863696+aalshoaibi@users.noreply.github.com>
2021-03-15 01:51:17 +0000

    test

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
+
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
 
 
 app.get('/', (req, res) => {
@@ -7,9 +16,62 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, ])
+    res.send(courses);
 
 });
+app.post('/api/courses', (req, res) => {
+
+
+    const { error } = validateCourse(req.body);
+
+    if (
+        error) {
+        res.status(400).send(
+            error
+        );
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+})
+
+app.get('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course was not found');
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course was not found');
+
+    const { error } = validateCourse(req.body);
+
+    if (
+        error) {
+        res.status(400).send(
+            error
+        );
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+
+
+
+})
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 
